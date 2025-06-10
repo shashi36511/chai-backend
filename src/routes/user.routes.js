383673960768c1a930e2690d162ecbca72c06aff
shashi36511 <shashi36511@gmail.com>
2025-06-10 11:09:37 +0545

    bug fixed at change-password route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -39,7 +39,7 @@ router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
 
-router.route("/change-password".post(verifyJWT, changeCurrentPassword))
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
